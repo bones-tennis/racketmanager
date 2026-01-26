c2e3652049b96d738c97fecba8fbd969df3d9d39
24B2067 <24B2067@10.30.112.164>
2026-01-26 13:36:53 +0900

    a

---
+++ racketmanager/src/main/resources/static/js/side_menu.js
@@ -0,0 +1,8 @@
+function openMenu(){
+  document.getElementById('sideMenu')?.classList.add('open');
+  document.getElementById('overlay')?.classList.add('show');
+}
+function closeMenu(){
+  document.getElementById('sideMenu')?.classList.remove('open');
+  document.getElementById('overlay')?.classList.remove('show');
+}
